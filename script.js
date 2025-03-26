98df2ea407375c10cbb9965221bd99d8348b5e7f
LuckyDreams16 <mrmarino11@hotmail.com>
2025-03-26 23:56:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const navLinks = document.getElementById('nav-links');
 mobileMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Lägg till eller ta bort klassen "active"
 });
+<script>
+  function toggleInfo() {
+    const info = document.getElementById("mer-info");
+    const button = document.getElementById("toggle-button");
+
+    if (info.style.display === "none") {
+      info.style.display = "block";
+      button.textContent = "Visa Mindre";
+    } else {
+      info.style.display = "none";
+      button.textContent = "Läs Mer";
+    }
+  }
+</script>
