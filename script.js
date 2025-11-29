0058cac2ccd1e458aacb524fb2f353d9b9aa57f7
LuckyDreams16 <mrmarino11@hotmail.com>
2025-11-29 10:54:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ const navLinks = document.getElementById('nav-links');
 mobileMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Lägg till eller ta bort klassen "active"
 });
- 
+ document.addEventListener("DOMContentLoaded", function () {
+    if (!localStorage.getItem("cookiesAccepted")) {
+        document.getElementById("cookie-banner").style.display = "block";
+    }
+
+    document.getElementById("cookie-accept").addEventListener("click", function () {
+        localStorage.setItem("cookiesAccepted", "true");
+        document.getElementById("cookie-banner").style.display = "none";
+    });
+});
+
  
